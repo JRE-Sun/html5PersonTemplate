89378a5f944a122ebbce116d104b42576d2a1b53
JRE-Sun <jresun@163.com>
2016-12-09 13:20:43 +0800

    了解基本的文字图片动画
    了解基本的文字图片动画，但是不包括导航其他的，仅仅只是文字，图片。

---
+++ js/index.js
@@ -0,0 +1,9 @@
+$(function() {
+    // alert("<s></s>");
+    $('#aa').waypoint(function() {
+        // alert('B5教程网 www.bcty365.com');
+        $("#img1").fadeToggle("slow");
+    }, { offset: -304 });
+
+
+});
